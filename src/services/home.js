58fa13d320ffd35c5d089c4ff046c334cd04b67e
ext.liuweijian3 <ext.liuweijian3@jd.com>
2025-12-26 13:30:12 +0800

    feat(ascf): 调整代码

--- src/services/home.js
+++ src/services/home.js
@@ -1,7 +1,162 @@
-import { mockData } from './data';
+export const data = {
+  categoryList: [
+    {
+      id: 1,
+      title: '超市',
+      imgUrl: 'https://img12.360buyimg.com/babel/jfs/t20270715/38278/23/22574/7960/6694edb4F07db03e3/d663cd498321eadc.png'
+    },
+    {
+      id: 2,
+      title: '电器',
+      imgUrl: 'https:///m.360buyimg.com/babel/jfs/t20270715/237082/37/21845/7616/6694edddFc764124a/38d00b686257b0f4.png'
+    },
+    {
+      id: 3,
+      title: '服饰美妆',
+      imgUrl: 'https://img20.360buyimg.com/babel/jfs/t20270715/36751/25/21385/7651/6694ee02F878cddef/13ce837dd39ad1ad.png'
+    },
+    {
+      id: 4,
+      title: '充值中心',
+      imgUrl: 'https://img20.360buyimg.com/babel/jfs/t20270715/44839/8/24550/7935/6694ee27F8775a577/b63c6a2fa0327964.png'
+    },
+    {
+      id: 5,
+      title: '生鲜',
+      imgUrl: 'https://m.360buyimg.com/babel/jfs/t20270715/22456/27/20943/10381/6694ee81F684396bb/0ba51f592d28dfdd.png'
+    },
+    {
+      id: 6,
+      title: '进口国际',
+      imgUrl: 'https://img11.360buyimg.com/babel/jfs/t20270715/29760/28/21267/11992/6694eea3F0fe3dca2/d5672661722bfc42.png'
+    },
+    {
+      id: 7,
+      title: '充值中心',
+      imgUrl: 'https://img20.360buyimg.com/babel/jfs/t20270715/44839/8/24550/7935/6694ee27F8775a577/b63c6a2fa0327964.png'
+    },
+    {
+      id: 8,
+      title: '生鲜',
+      imgUrl: 'https://m.360buyimg.com/babel/jfs/t20270715/22456/27/20943/10381/6694ee81F684396bb/0ba51f592d28dfdd.png'
+    },
+    {
+      id: 9,
+      title: '进口国际',
+      imgUrl: 'https://img11.360buyimg.com/babel/jfs/t20270715/29760/28/21267/11992/6694eea3F0fe3dca2/d5672661722bfc42.png'
+    },
+    {
+      id: 10,
+      title: '电器',
+      imgUrl: 'https:///m.360buyimg.com/babel/jfs/t20270715/237082/37/21845/7616/6694edddFc764124a/38d00b686257b0f4.png'
+    }
+  ],
+  imgList: [
+    {
+      id: 1,
+      imgUrl: 'https://m.360buyimg.com/mobilecms/s750x750_jfs/t1/255734/2/25535/120378/67be7d79Ffdd7af93/d37b11e2887fc54f.jpg'
+    },
+    {
+      id: 2,
+      imgUrl: 'https://m.360buyimg.com/mobilecms/s750x750_jfs/t1/357651/12/9794/79097/6916df9eF643a9687/9f74986f387b7c01.jpg'
+    },
+    {
+      id: 3,
+      imgUrl: 'https://m.360buyimg.com/mobilecms/s1125x1125_jfs/t1/237174/30/7574/74821/6579635eF65eca9bf/5e0bd1820d5c50e7.jpg'
+    }
+  ],
+  promotion: {
+    goodsList: [
+      {
+        id: 1,
+        cover: 'https://m.360buyimg.com/mobilecms/s1125x1125_jfs/t1/317073/5/1451/74194/68283fbaF78dca0a7/0f8c4d139b4e5b98.jpg',
+        price: 100,
+        name: '纯血鸿蒙HARMONYOS NEXT原生开发之旅',
+        sales: 246
+      },
+      {
+        id: 2,
+        cover: 'https://m.360buyimg.com/mobilecms/s750x750_jfs/t1/319261/19/19460/103089/68872a42F38e19ba2/2fd48e500d188a19.jpg',
+        price: 200,
+        name: 'HarmonyOS NEXT进阶 : 纯血鸿蒙开发实践(博文视点出品)',
+        sales: 350
+      }
+    ],
+    banner: '	https://developer.huawei.com/allianceCmsResource/resource/HUAWEI_Developer_VUE/images/0621227.jpg'
+  },
+  flashSale: {
+    goodsList: [
+      {
+        id: 3,
+        cover: 'https://img12.360buyimg.com/n2/s270x270_jfs/t1/187386/30/50437/169241/673c50b7Fed849f02/e3a87e14b9fe5ad8.jpg',
+        price: 899,
+        name: '鸿蒙HarmonyOS应用开发 全13册 HarmonyOS NEXT启程 零基础构建纯血鸿蒙应用+鸿蒙操作系统应用开发实践+鸿蒙操作系统开发入门经典+鸿蒙手机应用开发实战 计算机技术开发与应用丛书',
+        sales: 387
+      },
+      {
+        id: 4,
+        cover: 'https://img12.360buyimg.com/n2/s270x270_jfs/t1/192431/18/38016/125936/6508026dF9d31e62d/393bc114da06d6fa.jpg',
+        price: 56.4,
+        name: '鸿蒙App案例开发实战——生活应用与游戏开发30例（计算机科学与技术丛书）',
+        sales: 786
+      },
+      {
+        id: 5,
+        cover: 'https://img14.360buyimg.com/n2/s270x270_jfs/t1/229787/37/25917/167558/676291c3F0361338b/e68bf8ad2aa2cf03.jpg',
+        price: 46.4,
+        name: '【出版社直发】鸿蒙移动应用软件开发 华为鸿蒙系统HarmonyOS移动终端开发编程语言计算机软件开发书籍 广东人民出版社 鸿蒙移动应用软件开发',
+        sales: 230
+      },
+      {
+        id: 6,
+        cover: 'https://img10.360buyimg.com/n2/s270x270_jfs/t1/291345/2/6739/105705/682bd7a2Ff2d9f751/3128e8c7aa2c84cf.jpg',
+        price: 92,
+        name: '鸿蒙HarmonyOS NEXT开发之路 卷2：从入门到应用篇',
+        sales: 346
+      }
+    ],
+    banner: 'https://developer.huawei.com/allianceCmsResource/resource/HUAWEI_Developer_VUE/images/0603public/kf-banner-pc.jpeg'
+  },
+  recommend: {
+    goodsList: [
+      {
+        id: 7,
+        cover: 'https://img12.360buyimg.com/n2/s270x270_jfs/t1/257795/8/21947/107237/67b5a517F907482c4/05418a60a63e82f7.jpg',
+        price: 58.6,
+        sales: 88,
+        name: '鸿蒙HarmonyOS NEXT开发之路 卷1：ArkTS语言篇'
+      },
+      {
+        id: 8,
+        cover: 'https://img13.360buyimg.com/n2/s270x270_jfs/t1/324745/32/6612/73031/68a2d524F539da18a/f5011879cae7110c.jpg',
+        price: 49.3,
+        sales: 987,
+        name: '鸿蒙应用开发案例实战（ArkTS版）（AI助学）（微课版）'
+      },
+      {
+        id: 9,
+        cover: 'https://img14.360buyimg.com/n2/s270x270_jfs/t1/20956/17/20563/72057/6690ec97F501727f9/ac223ff455097d4c.jpg',
+        price: 52.9,
+        sales: 65,
+        name: '鸿蒙应用低代码开发'
+      },
+      {
+        id: 10,
+        cover: 'https://img13.360buyimg.com/n2/s270x270_jfs/t1/137400/32/23144/247054/621c4aecE4159e8e4/934fdfff3634f6e6.jpg',
+        price: 1000,
+        sales: 55,
+        name: '鸿蒙应用开发入门'
+      }
+    ]
+  }
+};
 
 
 // 获取首页数据
-const getHomeDataApi = () => Promise.resolve(mockData);
+const getHomeDataApi = () => Promise.resolve({
+  success: true,
+  status: 200,
+  data
+});
 
 export { getHomeDataApi };
